e584984d01053b8b36d41a2e63b7da0eaa489111
Luca Pasini <luca.pasini8@studio.unibo.it>
2024-05-21 19:10:26 +0200

    add messagges class

---
+++ src/main/java/pcd/part1/simengine_conc/message/MasterContenxt.java
@@ -0,0 +1,20 @@
+package pcd.part1.simengine_conc.message;
+
+public abstract class MasterContenxt {
+
+    public static final class InitSimulation extends MasterContenxt {
+
+    }
+    public static final class FinishStep extends MasterContenxt {
+
+    }
+    public static final class StopSimulation extends MasterContenxt {
+
+    }
+    public static final class FinishSimulation extends MasterContenxt {
+
+    }
+
+
+
+}
---
+++ src/main/java/pcd/part1/simengine_conc/message/WorkerContext.java
@@ -0,0 +1,7 @@
+package pcd.part1.simengine_conc.message;
+
+public class WorkerContext {
+    public static final class DoStep extends WorkerContext {
+
+    }
+}
