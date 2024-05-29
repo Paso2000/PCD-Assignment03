e59e6e87871e39d6232b573d0304a278d7f04448
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2024-05-29 16:00:45 +0200

    remove project in scala

--- sbt-subproject/src/main/scala/it/unibo/pcd/akka/Message.scala
+++
@@ -1,3 +0,0 @@
-package it.unibo.pcd.akka
-
-trait Message
--- sbt-subproject/src/main/scala/it/unibo/pcd/akka/cluster/advanced/Roles.scala
+++
@@ -1,5 +0,0 @@
-package it.unibo.pcd.akka.cluster.advanced
-
-object Roles:
-  val frontend: String = "frontend"
-  val backend: String = "backend"
