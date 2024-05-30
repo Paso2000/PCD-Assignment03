c8c626a5698b53074ec3b3e54e5ab3f56f326d0c
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2024-05-30 10:28:36 +0200

    implement GridActor

--- src/main/java/pcd/part2A/GridActor.java
+++ src/main/java/pcd/part2A/GridActor.java
@@ -15,11 +15,31 @@ public class GridActor extends AbstractBehavior<GridActorContext> {
     @Override
     public Receive<GridActorContext> createReceive() {
         return newReceiveBuilder()
-                .onMessage(GridActorContext.class, this::onInit)
+                .onMessage(GridActorContext.UpdateCell.class, this::onUpdateCell)
+                .onMessage(GridActorContext.SelectCell.class, this::onSelectCell)
+                .onMessage(GridActorContext.PlayerLefted.class, this::onPlayerLeft)
+                .onMessage(GridActorContext.PlayerJoined.class, this::onPlayerJoin)
                 .build();
     }
 
-    private Behavior<GridActorContext> onInit(GridActorContext gridActorContext) {
+    private Behavior<GridActorContext> onPlayerJoin(GridActorContext.PlayerJoined playerJoined) {
+        //notify all player che un altro player è entrato
         return null;
     }
+
+    private Behavior<GridActorContext> onPlayerLeft(GridActorContext.PlayerLefted playerLefted) {
+        //notify all player che un altro player è uscito
+        return null;
+    }
+
+    private Behavior<GridActorContext> onSelectCell(GridActorContext.SelectCell selectCell) {
+        //fa la select a tutti i player della cella
+        return null;
+    }
+
+    private Behavior<GridActorContext> onUpdateCell(GridActorContext.UpdateCell updateCell) {
+        //fa l'update del valore della cella per tutti i player
+    return null;
+    }
+
 }
