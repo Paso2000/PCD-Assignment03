404d0d9cefaf454048b00e91d83032d5362c2e95
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2024-05-30 12:01:07 +0200

    impostato implementazione attori

--- src/main/java/pcd/part2A/CoordinatorActor.java
+++ src/main/java/pcd/part2A/CoordinatorActor.java
@@ -1,9 +1,11 @@
 package pcd.part2A;
 
+import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Receive;
 import pcd.part2A.messages.CoordinatorActorContext;
+import pcd.part2A.messages.PlayerActorContext;
 
 public class CoordinatorActor extends AbstractBehavior<CoordinatorActorContext> {
     public CoordinatorActor(ActorContext<CoordinatorActorContext> context) {
@@ -12,6 +14,26 @@ public class CoordinatorActor extends AbstractBehavior<CoordinatorActorContext> 
 
     @Override
     public Receive<CoordinatorActorContext> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(CoordinatorActorContext.CreateGame.class, this::onCreateGame)
+                .onMessage(CoordinatorActorContext.JoinGame.class, this::onJoinGame)
+                .onMessage(CoordinatorActorContext.LeaveGame.class, this::onLeaveGame)
+                .build();
+    }
+
+    private Behavior<CoordinatorActorContext> onLeaveGame(CoordinatorActorContext.LeaveGame leaveGame) {
+        //rimuovo giocatore dalla partita
         return null;
     }
+
+    private Behavior<CoordinatorActorContext> onJoinGame(CoordinatorActorContext.JoinGame joinGame) {
+        //inserisco un giocatore nella partita
+        return null;
+    }
+
+    private Behavior<CoordinatorActorContext> onCreateGame(CoordinatorActorContext.CreateGame createGame) {
+        //creo un nuova partita con la mappa
+        return null;
+    }
+
 }
--- src/main/java/pcd/part2A/PlayerActor.java
+++ src/main/java/pcd/part2A/PlayerActor.java
@@ -1,8 +1,10 @@
 package pcd.part2A;
 
+import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Receive;
+import pcd.part2A.messages.GridActorContext;
 import pcd.part2A.messages.PlayerActorContext;
 
 public class PlayerActor extends AbstractBehavior<PlayerActorContext> {
@@ -12,6 +14,31 @@ public class PlayerActor extends AbstractBehavior<PlayerActorContext> {
 
     @Override
     public Receive<PlayerActorContext> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(PlayerActorContext.CellSelected.class, this::onCellSelected)
+                .onMessage(PlayerActorContext.NotifyPlayerJoined.class, this::onNotifyPlayerJoined)
+                .onMessage(PlayerActorContext.CellUpdated.class, this::onCellUpdated)
+                .onMessage(PlayerActorContext.NotifyPlayerLefted.class, this::onNotifyPlayerLefted)
+                .build();
+    }
+
+    private Behavior<PlayerActorContext> onNotifyPlayerLefted(PlayerActorContext.NotifyPlayerLefted notifyPlayerLefted) {
+        //visualizza che un giocatore ha lasciato
+        return null;
+    }
+
+    private Behavior<PlayerActorContext> onCellUpdated(PlayerActorContext.CellUpdated cellUpdated) {
+        //viusalizza il nuovo valore della cella
+        return null;
+    }
+
+    private Behavior<PlayerActorContext> onNotifyPlayerJoined(PlayerActorContext.NotifyPlayerJoined notifyPlayerJoined) {
+        //visualizza che un player si è unito
+        return null;
+    }
+
+    private Behavior<PlayerActorContext> onCellSelected(PlayerActorContext.CellSelected cellSelected) {
+        //visualizza che una cella è selezionata
         return null;
     }
 }
